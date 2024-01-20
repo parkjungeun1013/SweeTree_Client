72a220a7ec37340569b547714fbb0a2e87b9515e
parkjungeun1013 <lisa001013@naver.com>
2024-01-21 02:12:11 +0900

    Feat: 중간 글 작성 , 버튼 배치

--- sweetree_front/src/pages/Main1Page/Main1Page.tsx
+++ sweetree_front/src/pages/Main1Page/Main1Page.tsx
@@ -1,6 +1,9 @@
 import logo from "src/assets/logo.svg";
 import { Background } from "src/components/common/Background/Background";
-import * as S from "src/pages/LoginPage/LoginPage.styled";
+import * as S from "./Main1Page.styled";
+import { BrownButton } from "src/components/common/Button/BrownButton/BrownButton";
+
+
 
 export const Main1Page = () => {
     return (
@@ -8,7 +11,21 @@ export const Main1Page = () => {
             <S.LogoPos>
                 <img src = {logo} alt = 'logo' />
             </S.LogoPos>
-            <p>안녕</p>
+
+            <S.TextSytle>
+                고마워 <br /> 미안해 <br /> 좋아해 <br /> ... <br/>
+                미처 전하지 못한 마음이 있나요? <br />
+                잊혀진 달콤함을 배달합니다 <br /> <br />
+                2월 14일 <br /> 세상에서 가장 달콤한 편지를 받아보시려면 
+            </S.TextSytle>
+        
+            <BrownButton 
+            title = {"내 초콜릿 상자 만들기"}
+            width ={235.266}
+            />
+
         </Background>
+        
+        
     )
 }
